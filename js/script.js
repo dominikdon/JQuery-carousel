d755c91357a381ec57d9b1cfed4d1a7ba915c150
Dominik Doniec <dominikdoniec@gmail.com>
2018-09-18 22:52:49 +0200

    Add scroll buttons. Modify JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 $(function(){
-
-  var carouselList = $('#carousel ul');
-
-    setInterval(changeSlides, 4000);
-    function changeSlides(){
-      carouselList.animate({'marginLeft':-600}, 600, moveFirstSlide);  
-    };
-
+var carouselList = $('#carousel ul');
+  
 function moveFirstSlide(){
   var firstItem = carouselList.find('li:first');
   var lastItem = carouselList.find('li:last');
   lastItem.after(firstItem),
-   carouselList.css({marginLeft:0});
+  carouselList.css({marginLeft:0});
 };
+  
+function moveLastSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft:-600});
+  };  
+
+function changeSlideRight(){
+    carouselList.animate({'marginLeft':-600}, 600, moveFirstSlide);  
+    };
+  
+function changeSlideLeft(){
+    carouselList.animate({'marginLeft':0}, 600, moveLastSlide)
+    };
+  
+var timer = function(){
+    interval = setInterval(changeSlideRight, 3000);
+};
+  
+timer();
+  
+$(".previous").click(function(){
+     clearInterval(interval);
+     changeSlideLeft();
+     timer()
+  });
+  
+ $(".next").click(function(){
+    clearInterval(interval);
+    changeSlideRight();
+    timer()
+  });
 });
